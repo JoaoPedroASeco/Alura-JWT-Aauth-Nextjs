23ea30ed72fa28be6d4b965a9c6580365ffa89d7
Guilherme Lima <guilherme.lima@alura.com>
2022-03-25 11:48:18 -0300

    aula_1

---
+++ frontend/pages/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    usuario: 'omariosouto',
+    senha: 'safepassword',
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    })
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+
+        // router.push('/auth-page-static');
+        router.push('/auth-page-ssr');
+      }}>
+        <input
+          placeholder="Usuário" name="usuario"
+          value={values.usuario} onChange={handleChange}
+        />
+        <input
+          placeholder="Senha" name="senha" type="password"
+          value={values.senha} onChange={handleChange}
+        />
+        {/* <pre>
+          {JSON.stringify(values, null, 2)}
+        </pre> */}
+        <div>
+          <button>
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
